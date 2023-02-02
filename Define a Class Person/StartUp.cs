63bea058162784eef077199efcb8cfc4a082c39e
Nikolay Angelov <108483720+nikolaygangelov@users.noreply.github.com>
2023-02-02 20:26:51 +0200

    Add files via upload

---
+++ Define a Class Person/StartUp.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace DefiningClasses
+{
+   public class StartUp
+    {
+        static void Main(string[] args)
+        {
+
+            int N = int.Parse(Console.ReadLine());
+            Family family = new Family();
+            for (int i = 0; i < N; i++)
+            {
+                string [] namesAndAges = Console.ReadLine()
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                string nameFromConsole = namesAndAges[0];
+                int ageFromConsole = int.Parse(namesAndAges[1]);
+
+                Person person = new Person
+                {
+                    Name = nameFromConsole,
+                    Age = ageFromConsole
+                };
+                family.AddMember(person);
+            }
+
+            Person old = old = family.GetOldestMember();
+
+            Console.Write($"{old.Name} {old.Age}");
+
+            
+        }
+    }
+}
