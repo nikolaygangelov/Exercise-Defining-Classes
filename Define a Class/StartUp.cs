cec5342e72d5bd2c9a70788b36612741a3266545
Nikolay Angelov <108483720+nikolaygangelov@users.noreply.github.com>
2023-02-03 00:24:48 +0200

    Add files via upload

---
+++ Define a Class/StartUp.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+   public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Person peter = new Person
+            {
+                Name = "Peter",
+                Age = 20
+            };
+            Person george = new Person
+            {
+                Name = "George",
+                Age = 18
+            };
+            Person jose = new Person
+            {
+                Name = "Jose",
+                Age = 43
+            };
+
+            Console.WriteLine($"{peter.Name} is {peter.Age} years old");
+            Person noName = new Person("gosho", 36);
+            Console.WriteLine($"{noName.Name} is {noName.Age} years old");
+        }
+    }
+}
